03cfa8dc2db96b5c4b5ee9cccbca1b886feca25b
Baeck Dong Jae <161856415+VectorSophie@users.noreply.github.com>
2025-10-13 11:26:18 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Neon Portfolio',
+  title: 'Jack B. Github page',
   description: 'A personal portfolio for Sophie Vector.',
 };
 
